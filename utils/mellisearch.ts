ba0606ea2b38619ddd1417128379056970d8ea88
johnkryptochain <johncrypto@protonmail.ch>
2023-05-03 18:46:37 +0200

    update

--- utils/mellisearch.ts
+++ utils/mellisearch.ts
@@ -1,6 +1,5 @@
 import { MeiliSearch, Index } from 'meilisearch'
 
-
 const searchClient = new MeiliSearch({
     host: 'https://ms-cb4689884309-3570.sfo.meilisearch.io',
     apiKey: 'b0cc5c308cb3e2915ce8635cf0105ea1fb07bacb0e619b3e8b4d2e8f820fb75b',
@@ -19,7 +18,7 @@ searchClient.getIndex('users')
     })
     .catch((error: any) => {
         if (error.errorCode === 'index_not_found') {
-            searchClient.createIndex({ uid: 'users' }).then((index: Index) => {
+            searchClient.createIndex('users').then((index: Index) => {
                 index.updateSettings({
                     searchableAttributes: [
                         'name'
